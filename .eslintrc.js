9c9b8642e4257aa2a1117b1f7cbb39b4b5a786dd
Kus Cámara <kcmr@users.noreply.github.com>
2019-03-16 12:25:38 +0100

    chore(eslint): config rules for node

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,33 +6,16 @@ module.exports = {
 		ecmaVersion: 2017,
 		sourceType: 'module'
 	},
-	plugins: [
-		'ember'
-	],
 	extends: [
 		'eslint-config-bbva'
 	],
 	env: {
-		browser: true
+		browser: false,
+		node: true
 	},
-	overrides: [{
-		files: [
-			'.commitlintrc.js',
-			'.eslintrc.js',
-			'index.js'
-		],
-		excludedFiles: [],
-		parserOptions: {
-			sourceType: 'script',
-			ecmaVersion: 2015
-		},
-		env: {
-			browser: false,
-			node: true
-		},
-		plugins: ['node'],
-		rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
-			'no-process-env': 0
-		})
-	}]
+	plugins: ['node'],
+	rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
+		'no-process-env': 0,
+		'no-sync': 0
+	})
 };
