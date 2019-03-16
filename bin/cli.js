3eb89eadcf3b3bdc4c066001efd60fa116cd120c
Kus Cámara <kcmr@users.noreply.github.com>
2019-03-16 12:49:57 +0100

    feat(cli): throw error if there is no config file

--- bin/cli.js
+++ bin/cli.js
@@ -3,10 +3,15 @@
 'use strict';
 
 const CONFIG_FILE = '.polymerprojectrc';
-const fs = require('fs');
+const fs = require('fs-extra');
 const findUp = require('find-up');
-const configPath = findUp.sync(CONFIG_FILE);
-const config = configPath ? JSON.parse(fs.readFileSync(configPath)) : {};
+const configFilePath = findUp.sync(CONFIG_FILE);
+
+if (!configFilePath) {
+	throw new Error(`No config file found in a ${CONFIG_FILE} file`);
+}
+
+const config = fs.readJSONSync(configFilePath);
 const PolymerProjectBuilder = require('../lib/index');
 
 const builder = new PolymerProjectBuilder(config);
