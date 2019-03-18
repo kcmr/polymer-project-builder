89baa3a114782a07328bd8d1376e4c9c42450af1
Kus Cámara <kcmr@users.noreply.github.com>
2019-03-18 17:15:26 +0100

    chore(jest.config): change coverage thresholds

--- jest.config.js
+++ jest.config.js
@@ -10,10 +10,10 @@ module.exports = {
 	],
 	coverageThreshold: {
 		global: {
-			branches: 85,
-			functions: 85,
-			lines: 85,
-			statements: 85
+			branches: 100,
+			functions: 100,
+			lines: 100,
+			statements: 100
 		}
 	}
 };
